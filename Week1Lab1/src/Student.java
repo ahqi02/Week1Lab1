0bac6f6cd45789176a7949e784a8bfa743a8b6f8
Chua Hui Qi <Chua Hui Qi@10.102.26.71>
2022-10-15 09:50:51 +0800

    Commit 3 classes

---
+++ Week1Lab1/src/Student.java
@@ -0,0 +1,13 @@
+
+public class Student {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Name: Chua Hui Qi");
+		System.out.println("Matric No.: 211917");
+		System.out.println("Address: 123, Jln Merdeka, 81100 Johor Bahru, Johor.");
+
+	}
+
+}
